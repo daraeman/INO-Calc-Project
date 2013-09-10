696fd04c7249f85673f53cf7d9d272647472e07d
daraeman <daraeman@gmail.com>
2013-09-10 03:00:34 -0700

    fixes #3; made python a little faster with numpy random array

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ The INO Calc Project
 
 For some reason I decided to implement [calc.innoutchallenge.com](http://calc.innoutchallenge.com) in way more languages than necessary.
 
-Currently written in:
+Currently written in (stats are for 100,000 iterations):
 
-* Ruby (100000 iterations in 101.961446 seconds)
-* Python (100000 iterations in 176.089699984 seconds)
-* Java (100000 iterations in 1.482267 seconds)
-* PHP (100000 iterations in 31.7638840675 seconds)
-* Javascript (100000 iterations in 1.1129259999979695 seconds)
-* C (100000 iterations in 1.125626 seconds)
+* Ruby (101.961446 seconds)
+* Python (Vanilla: 176.089699984 seconds, NumPy: 74.9039349556 seconds)
+* Java (1.482267 seconds)
+* PHP (31.7638840675 seconds)
+* Javascript (1.1129259999979695 seconds)
+* C (1.125626 seconds)
 
 *Feel free to correct any mistakes. There's prolly a ton.*
---
+++ python/ino_numpy.py
@@ -0,0 +1,62 @@
+# start timing
+import time
+start = time.time()
+
+# import modules
+import array
+import random
+import numpy as np
+
+# initialize the totals array
+totals = array.array( 'i' , (0 for i in range(0,2) ) )
+numpyrand = np.random.randint(1,98, size=5001)
+numpypos = 0
+
+# loop
+for i in range(100000):
+
+    # initialize the receipts array
+    receipts = array.array( 'i' , (0 for i in range(0,98) ) )
+    iterations = 0
+    done = 98
+
+    # loop until 'done' has been subtracted to zero
+    while (done > 0):
+
+        # generate random number
+        #newnum = numpy.random.randint(0,97)
+
+        if (numpypos > 5000) :
+            numpyrand = np.random.randint(0,98, size=5001)
+            numpypos = 0
+
+        # if the number is new then mark it as seen
+        if receipts[ numpyrand[numpypos] ] == 0:
+            receipts[ numpyrand[numpypos] ] = 1
+
+            # decrease 'done'
+            done -= 1
+
+                # increase this round's count
+        iterations += 1
+        numpypos += 1
+
+    # add one to the totals for this number
+    totalslength = len(totals)
+
+    # if the array index has been defined increment the value
+    if iterations <= totalslength:
+        totals[iterations] += 1
+
+    # else add in zero values for all the indices up to the new value and then add the new value as one
+    else:
+        for newindex in range( totalslength , iterations ) :
+            totals.append(0);
+        totals.append(1)
+
+# remove the first elements since the array is zero-based but the range starts at 1
+totals.pop(0);
+print ", ".join([str(x) for x in totals])
+#print "Total Iterations", sum(totals)
+
+print sum(totals), 'iterations in', time.time()-start, 'seconds.'
